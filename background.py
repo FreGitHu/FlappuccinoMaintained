84529f48ddd304b98300e0b44f47987a85ba644c
FreGitHu <101549035+FreGitHu@users.noreply.github.com>
2024-02-24 13:40:02 +0000

    remove even more pesky spaces

--- background.py
+++ background.py
@@ -5,7 +5,7 @@ class Background:
         self.sprite = pygame.image.load('data/gfx/bg.png')
         self.position = 0
         self.uncoloredSprite = pygame.image.load('data/gfx/bg.png')
-    def setSprite(self, tint):  
+    def setSprite(self, tint):
         copy = self.uncoloredSprite.copy()
         color = colorsys.hsv_to_rgb(tint,1,1)
         copy.fill((color[0]*255, color[1]*255, color[2]*255), special_flags=pygame.BLEND_ADD)
